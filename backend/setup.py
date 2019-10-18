6bb628df0268595db15f3d324d0dbc58e83cb343
Kazuki Oikawa <k@oikw.org>
2019-10-19 00:15:27 +0900

    backend: setup.pyの記述ミスを修正 (packages引数不足)

--- backend/setup.py
+++ backend/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from configparser import ConfigParser
 import json
 
@@ -15,6 +15,7 @@ install_requires = [
 setup(
     name='penguin_judge',
     version='0.0.1',
+    packages=find_packages(),
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
