edd0eea309597096b5fb8d6a07bc4ec62a11e5db
y-i <y-i@users.noreply.github.com>
2019-12-10 15:30:08 +0900

    frontend: 提出結果に色づけして結果を分かりやすいよう修正

--- frontend/src/views/contest-submission.ts
+++ frontend/src/views/contest-submission.ts
@@ -2,7 +2,7 @@ import { customElement, LitElement, html, css } from 'lit-element';
 import { Subscription, zip } from 'rxjs';
 import { API, Submission } from '../api';
 import { session } from '../state';
-import { format_datetime_detail } from '../utils';
+import { format_datetime_detail, getSubmittionStatusClass } from '../utils';
 
 @customElement('penguin-judge-contest-submission')
 export class PenguinJudgeContestSubmission extends LitElement {
@@ -41,7 +41,7 @@ export class PenguinJudgeContestSubmission extends LitElement {
       return;
     }
     const tests = this.submission.tests.map(
-      t => html`<tr><td>${t.id}</td><td>${t.status}</td><td>${t.time === undefined ? '-' : t.time}</td><td>${t.memory === undefined ? '-' : t.memory}</td></tr>`);
+      t => html`<tr><td>${t.id}</td><td class="${getSubmittionStatusClass(t.status)}">${t.status}</td><td>${t.time === undefined ? '-' : t.time}</td><td>${t.memory === undefined ? '-' : t.memory}</td></tr>`);
 
     return html`
       <h2>提出: #${this.submission.id}</h2>
@@ -53,7 +53,7 @@ export class PenguinJudgeContestSubmission extends LitElement {
         <tr><th>問題</th><td>${this.submission.problem_id}</td></tr>
         <tr><th>ユーザ</th><td>${this.submission.user_id}</td></tr>
         <tr><th>言語</th><td>${session.environment_mapping[this.submission.environment_id].name}</td></tr>
-        <tr><th>結果</th><td>${this.submission.status}</td></tr>
+        <tr><th>結果</th><td class="${getSubmittionStatusClass(this.submission.status)}">${this.submission.status}</td></tr>
       </table>
       <h3>テストケース</h3>
       <table class="testcase">
@@ -86,6 +86,12 @@ export class PenguinJudgeContestSubmission extends LitElement {
       table.testcase tbody tr td:nth-child(3+n) {
         text-align: right;
       }
+      .AC {
+        background-color: #86C166;
+      }
+      .WA {
+        background-color: #F05E1C;
+      }  
     `;
   }
 }
