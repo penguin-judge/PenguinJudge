2f12b5b88f1604ba3917027df97a9bbc868f16c3
Kazuki Oikawa <k@oikw.org>
2019-12-19 06:00:29 +0900

    backend: APIのサーバ実装をflask.runからgunicornに切り替え

--- backend/penguin_judge/main.py
+++ backend/penguin_judge/main.py
@@ -1,19 +1,24 @@
 from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
 from os import sched_getaffinity
-from typing import Mapping
+from typing import Any, Mapping
 
 from penguin_judge.models import configure, get_db_config
 from penguin_judge.mq import configure as configure_mq
 
 
-def _load_config(args: Namespace, name: str) -> Mapping[str, str]:
+def _load_config(args: Namespace, name: str,
+                 exclude_defaults: bool = False) -> Mapping[str, str]:
     config = ConfigParser()
     if args.config:
         config.read(args.config)
-    ret = config.defaults()
+    ret = dict(config.defaults())
     if config.has_section(name):
         ret = dict(config.items(name))
+    if exclude_defaults:
+        for k in config.defaults().keys():
+            ret.pop(k, None)
+        return ret
 
     require_keys = ('sqlalchemy.url', 'mq.url')
     for k in require_keys:
@@ -24,11 +29,24 @@ def _load_config(args: Namespace, name: str) -> Mapping[str, str]:
 
 
 def start_api(args: Namespace) -> None:
+    from gunicorn.app.base import BaseApplication  # type: ignore
     from penguin_judge.api import app
+
     config = _load_config(args, 'api')
-    configure(**config)
     configure_mq(**config)
-    app.run(host='0.0.0.0')
+
+    class App(BaseApplication):
+        def load_config(self) -> None:
+            config = _load_config(args, 'gunicorn', exclude_defaults=True)
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+
+        def load(self) -> Any:
+            # DBはプロセス単位で初期化する必要がある
+            configure(**config)
+            return app
+
+    App().run()
 
 
 def start_worker(args: Namespace) -> None:
