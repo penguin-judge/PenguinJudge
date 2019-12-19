fae669cb55b72d2b7c8df7a1f73df15ce08a6dd7
Kazuki Oikawa <k@oikw.org>
2019-12-19 11:37:38 +0900

    backend: 管理者の場合は開催前のコンテストの問題を取得できるように修正

--- backend/penguin_judge/api.py
+++ backend/penguin_judge/api.py
@@ -284,7 +284,7 @@ def get_contest(contest_id: str) -> Response:
         if not (contest and contest.is_accessible(u)):
             abort(404)
         ret = contest.to_dict()
-        if contest.is_begun():
+        if contest.is_begun() or (u and u['admin']):
             problems = s.query(Problem).filter(
                 Problem.contest_id == contest_id).all()
             if problems:
