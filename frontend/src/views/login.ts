b5226c35801f8cd3d00e25c5e608a4220aa63565
mikikusu <miki.sakai@gmail.com>
2019-12-23 20:02:03 +0900

    ログインとユーザ登録のデザイン修正
    CSS当ててみた。

--- frontend/src/views/login.ts
+++ frontend/src/views/login.ts
@@ -27,7 +27,7 @@ export class AppHomeElement extends LitElement {
         session.update_current_user();
       },
       err => {
-        if (err.status === 404) this.errorStr = 'ユーザ名またはパスワードが違います';
+        if (err.status === 404 || err.status === 400) this.errorStr = 'ユーザIDまたはパスワードが違います';
         if (err.status >= 500 && err.status <= 503) this.errorStr = 'サーバ側のエラーです';
         this.requestUpdate();
       },
@@ -42,37 +42,110 @@ export class AppHomeElement extends LitElement {
 
   render() {
     return html`
-      <x-panel header="ログイン">
+    <div class="form-wrapper">
+      <form>
+        <div class="form-item">
+          <label for="userid"></label>
+          <input id="userid" autofocus placeholder="ユーザID"></input>
+        </div>
+        <div class="form-item">
+          <label for="password"></label>
+          <input type="password" id="password" placeholder="パスワード"></input>
+        </div>
+        <div class="button-panel">
+          <button @click="${this.post}" class="button">ログイン</button>
+        </div>
+      </form>
+      <div class="form-footer">
+        <p><a is="router-link" href="${router.generate('register')}">Create a new account</a></p>
         ${this.errorStr !== null ?
         html`
-            <div>
+            <p style="color:red;">
               ${this.errorStr}
-            </div>` : html``}
-        <div>
-          <form>
-            <label>ユーザID:&nbsp;<input id="userid" autofocus>
-            <br>
-            <label>パスワード:&nbsp;<input id="password" type="password" minlength="8">
-            <br>
-            <button @click="${this.post}">ログイン</button>
-          </form>
-        </div>
-        <div>
-          <br>登録がまだの場合は<a is="router-link" href="${router.generate('register')}">こちら</a>
-        </div>
-      </x-panel>
+            </p>` : html``}
+      </div>
+    </div>
     `
   }
 
   static get styles() {
     return css`
       :host {
-        display: flex;
-        flex-direction: column;
         padding: ${MainAreaPaddingPx}px;
       }
-      x-panel {
-        margin-bottom: 20px;
+
+      * { margin: 0; padding: 0; box-sizing: border-box; }
+
+      .form-wrapper {
+        margin: 3em auto;
+        padding: 0 1em;
+        width: 370px;
+        border: 1px solid darkgrey;
+        border-radius: 10px;
+        font-size: 1em;
+      }
+
+      form {
+        padding: 0 1.5em;
+      }
+
+      .form-item {
+        margin-bottom: 0.7em;
+      }
+
+      .form-item:first-child {
+        margin-top: 2em;
+      }
+
+      .form-item input {
+        background: inherit;
+        border: none;
+        border-bottom: 2px solid #eee;
+        font-size: 1em;
+        height: 2em;
+        transition: border-color 0.3s;
+        width: 100%;
+      }
+
+      .form-item input:focus {
+        border-bottom-color: #ccc;
+        outline: none;
+      }
+
+      .button-panel {
+        margin: 1.5em 0 0;
+      }
+
+      .button-panel .button {
+        background: #6495ed;
+        border: none;
+        color: #fff;
+        cursor: pointer;
+        height: 50px;
+        font-size: 1.2em;
+        letter-spacing: 0.05em;
+        text-align: center;
+        transition: background 0.3s ease-in-out;
+        width: 100%;
+      }
+
+      .button:hover {
+        background: #4169e1;
+      }
+
+      .form-footer {
+        padding: 1em 0;
+        text-align: center;
+      }
+
+      .form-footer a {
+        color: #8c8c8c;
+        text-decoration: none;
+        transition: border-color 0.3s;
+      }
+
+      .form-footer a:hover {
+        border-bottom: 1px dotted #8c8c8c;
       }
     `
   }
--- frontend/src/views/register.ts
+++ frontend/src/views/register.ts
@@ -27,6 +27,7 @@ export class RegisterElement extends LitElement {
       router.navigate('login');
     } catch (err) {
       if (err.status === 409) this.errorMsg = '既に存在するユーザIDです';
+      else if (err.status === 400) this.errorMsg = '使用できない文字列が使われたり、パスワードが短すぎる可能性があります';
       else if (err.status >= 500 && err.status <= 503) this.errorMsg = 'サーバ側のエラーです';
       else this.errorMsg = err.status;
     } finally {
@@ -37,39 +38,115 @@ export class RegisterElement extends LitElement {
 
   render() {
     return html`
-      <x-panel header="ユーザ登録">
-        <form>
-            ${this.errorMsg !== null ?
+    <div class="form-wrapper">
+      <form>
+        <div class="form-item">
+          <label for="userid"></label>
+          <input id="userid" autofocus placeholder="ユーザID"></input>
+        </div>
+        <div class="form-item">
+          <label for="username"></label>
+          <input id="username" autofocus placeholder="ユーザ名（表示名）"></input>
+        </div>
+        <div class="form-item">
+          <label for="password"></label>
+          <input type="password" id="password" placeholder="パスワード(8文字以上)"></input>
+        </div>
+        <div class="button-panel">
+          <button @click="${this.post}" ?disabled="${this.isWaitingResponce}" class="button">ユーザ登録</button>
+        </div>
+      </form>
+      <div class="form-footer">
+        <p><a is="router-link" href="${router.generate('login')}">If you already registered.</a></p>
+        ${this.errorMsg !== null ?
         html`
-          <div>
-            ${this.errorMsg}
-          </div>
-          <br>` : html``}
-          <div>
-            <label>ユーザID:&nbsp;<input id="userid" autofocus></label>
-            <br>
-            <label>ユーザ名:&nbsp;<input id="username"></label>
-            <br>
-            <label>パスワード:&nbsp;<input id="password" type="password" minlength="8"></label>
-            <br>
-            <button @click="${this.post}" ?disabled="${this.isWaitingResponce}">登録</button>
-          </div>
-          <div>登録済みの場合は<a is="router-link" href="${router.generate('login')}">こちら</a></div>
-        </form>
-      </x-panel>
+            <p style="color:red;">
+              ${this.errorMsg}
+            </p>` : html``}
+      </div>
+    </div>
     `
   }
 
   static get styles() {
     return css`
-      :host {
-        display: flex;
-        flex-direction: column;
-        padding: ${MainAreaPaddingPx}px;
-      }
-      x-panel {
-        margin-bottom: 20px;
-      }
+    :host {
+      padding: ${MainAreaPaddingPx}px;
+    }
+
+    * { margin: 0; padding: 0; box-sizing: border-box; }
+
+    .form-wrapper {
+      margin: 3em auto;
+      padding: 0 1em;
+      width: 370px;
+      border: 1px solid darkgrey;
+      border-radius: 10px;
+      font-size: 1em;
+    }
+
+    form {
+      padding: 0 1.5em;
+    }
+
+    .form-item {
+      margin-bottom: 0.7em;
+    }
+
+    .form-item:first-child {
+      margin-top: 2em;
+    }
+
+    .form-item input {
+      background: inherit;
+      border: none;
+      border-bottom: 2px solid #eee;
+      font-size: 1em;
+      height: 2em;
+      transition: border-color 0.3s;
+      width: 100%;
+    }
+
+    .form-item input:focus {
+      border-bottom-color: #ccc;
+      outline: none;
+    }
+
+    .button-panel {
+      margin: 1.5em 0 0;
+    }
+
+    .button-panel .button {
+      background: #6495ed;
+      border: none;
+      color: #fff;
+      cursor: pointer;
+      height: 50px;
+      font-size: 1.2em;
+      letter-spacing: 0.05em;
+      text-align: center;
+      transition: background 0.3s ease-in-out;
+      width: 100%;
+    }
+
+    .button:hover {
+      background: #4169e1;
+    }
+
+    .form-footer {
+      padding: 1em 0;
+      text-align: center;
+    }
+
+    .form-footer a {
+      color: #8c8c8c;
+      text-decoration: none;
+      transition: border-color 0.3s;
+    }
+
+    .form-footer a:hover {
+      border-bottom: 1px dotted #8c8c8c;
+    }
     `
   }
 }
