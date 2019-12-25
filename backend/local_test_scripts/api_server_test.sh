fdd185c0da85dd83516793de4d3ae7983496c379
Kazuki Oikawa <k@oikw.org>
2019-12-25 17:21:44 +0900

    backend: もう動かないローカルテストスクリプトを削除

--- backend/local_test_scripts/api_server_test.sh
+++
@@ -1,4 +0,0 @@
-curl localhost:5000/user/2917cdeb49f938b
-curl -X POST -H 'Content-Type:application/json' -d "{\"name\":\"kazuki\",\"password\":\"1234\"}" localhost:5000/user
-curl localhost:5000/contests/1/problems/1/submission
-curl localhost:5000/contests/1/problems/1/submission/all
