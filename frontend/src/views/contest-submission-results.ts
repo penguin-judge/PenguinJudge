5987edfde05d6a3d8c3f8c9dafa8b4c38b079c54
Kazuki Oikawa <k@oikw.org>
2020-05-21 07:36:01 +0900

    frontend: 提出一覧ページにフィルタ機能を追加

--- frontend/src/views/contest-submission-results.ts
+++ frontend/src/views/contest-submission-results.ts
@@ -42,6 +42,16 @@ export class PenguinJudgeContestSubmissionResults extends LitElement {
   loadSubmissions() {
     const query = new Map<string, string>();
     query.set('page', this.page_index.toString());
+    {
+      [['problem-filter', 'problem_id'],
+       ['env-filter', 'environment_id'],
+       ['status-filter', 'status'],
+       ['user-filter', 'user_name']].forEach(([k0, k1]) => {
+         const element = <HTMLInputElement>this.shadowRoot!.getElementById(k0);
+         if (!element || element.value === '') return;
+         query.set(k1, element.value);
+       });
+    }
     if (this.order_key) {
       query.set('sort', this.order_key);
     }
@@ -72,9 +82,7 @@ export class PenguinJudgeContestSubmissionResults extends LitElement {
       return html`<div>コンテスト開催前です</div>`;
     }
 
-    let pagenation;
-    if (this.number_of_pages >= 1)
-      pagenation = html`<penguin-judge-pagenation pages="${this.number_of_pages}" currentPage="${this.page_index}" @page-changed="${this.changePage}"></penguin-judge-pagenation>`;
+    const pagenation = html`<penguin-judge-pagenation pages="${this.number_of_pages}" currentPage="${this.page_index}" @page-changed="${this.changePage}"></penguin-judge-pagenation>`;
 
     const nodes = this.submissions.map(s => {
       const url = router.generate('contest-submission', { id: session.contest!.id, submission_id: s.id });
@@ -94,7 +102,38 @@ export class PenguinJudgeContestSubmissionResults extends LitElement {
     return html`
       ${pagenation}
       <table id="submission_list">
-        <thead><tr>
+        <thead><tr><td colspan="9" id="filter-column"><div>
+            <label for="problem-filter">問題:</label>
+            <select id="problem-filter" @input="${this.loadSubmissions}">
+              <option value="" selected>-</option>
+              ${session.contest.problems.map(p => {
+                return html`<option value="${p.id}">${p.id}: ${p.title}</option>`;
+              })}
+            </select>
+            <label for="env-filter">言語:</label>
+            <select id="env-filter" @input="${this.loadSubmissions}">
+              <option value="" selected>-</option>
+              ${session.environments.map(e => {
+                return html`<option value="${e.id}">${e.name}</option>`;
+              })}
+            </select>
+            <label for="status-filter">結果:</label>
+            <select id="status-filter" @input="${this.loadSubmissions}">
+              <option value="" selected>-</option>
+              <option value="Accepted">AC</option>
+              <option value="WrongAnswer">WA</option>
+              <option value="TimeLimitExceeded">TLE</option>
+              <option value="MemoryLimitExceeded">MLE</option>
+              <option value="RuntimeError">RE</option>
+              <option value="CompilationError">CE</option>
+              <option value="OutputLimitExceeded">OLE</option>
+              <option value="Waiting">WJ</option>
+              <option value="Running">Judging</option>
+            </select>
+            <label for="user-filter">ユーザ:</label>
+            <input id="user-filter" type="text">
+            <button @click="${this.loadSubmissions}">フィルタ</button>
+          </div></td></tr><tr>
           <td><a @click="${() => this._sort('created')}">提出日時</a></td>
           <td>問題</td>
           <td>ユーザ</td>
@@ -135,6 +174,29 @@ export class PenguinJudgeContestSubmissionResults extends LitElement {
     td a:hover {
       cursor: pointer;
       text-decoration: underline;
+    }
+    tbody tr td:nth-child(n+6) {
+      text-align: right;
+    }
+    #filter-column * {
+      font-size: 85%;
+    }
+    #filter-column label {
+      margin-left: 1em;
+      margin-right: 2px;
+    }
+    #filter-column > div {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+    #filter-column input {
+      flex-grow: 1;
+      margin-right: 1em;
+    }
+    #filter-column button {
+      margin-right: 1em;
+      padding: 0;
     }
     `;
   }
